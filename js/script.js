3c832e86d1973edd6e87a409606e0acab35bd7dc
Alejandro Garcia Barajas <agarciab28@gmail.com>
2018-05-25 11:12:32 -0500

    1.0.1

--- js/script.js
+++ js/script.js
@@ -77,3 +77,42 @@ $(document).ready(function(){
   $('#cardsRedes').css( 'cursor', 'pointer' );
 
 });
+
+
+// Funciones para hacer funcionar la barra de aceptacion de cookies
+function getCookie(c_name){
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1){
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1){
+        c_value = null;
+    }else{
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1){
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value;
+}
+
+function setCookie(c_name,value,exdays){
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+}
+
+if(getCookie('bichon')!="1"){
+    document.getElementById("barraaceptacion").style.display="block";
+}
+else {
+  document.getElementById("barraaceptacion").style.display="none";
+}
+function PonerCookie(){
+    setCookie('bichon','1',365);
+    document.getElementById("barraaceptacion").style.display="none";
+}
